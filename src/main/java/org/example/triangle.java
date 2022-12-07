c27483c41923674de564404e85be82c11abfbe47
DmytroSa1997 <deanwincester1907@gmail.com>
2022-12-08 00:49:51 +0200

    Initial commit

---
+++ src/main/java/org/example/triangle.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.Scanner;
+public class triangle {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите ширину треугольника: ");
+        double base = scanner.nextDouble();
+
+        System.out.println("Введите высоту треугольника: ");
+        double height = scanner.nextDouble();
+
+        //Площадь треугольника вычисляется по формуле = (width*height)/2
+        double area = (base* height)/2;
+        System.out.println("Площадь треугольника равна: " + area);
+    }
+}
